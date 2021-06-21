a3a53fafe7626f000e89e59f5acb1c2f618296c6
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2021-06-22 00:22:12 +0100

    comment and cleanup

--- src/startup.lua
+++ src/startup.lua
@@ -1,22 +1,23 @@
 function installPrograms()
+    local list
     if fs.exists("programlist") then
-    
+        -- load programlist from file
     else
-        local default = 
-        {
-            udo = {link="cUwVrENR", path="bin"},
-            button = {link="x71BzgKb", path="api_dir"},
-            calc = {link="5v4aar72", path="bin"},
-            play2048 = {link="RHhdEZ52", path="bin"}
+        list = {
+            udo = {link = "cUwVrENR", path = "bin"},
+            button = {link = "x71BzgKb", path = "api_dir"},
+            calc = {link = "5v4aar72", path = "bin"},
+            play2048 = {link = "RHhdEZ52", path = "bin"}
         }
-
-        for name,cfg in pairs(default) do
-            shell.run(
+    end
+    -- Install every program
+    for name, cfg in pairs(list) do
+        shell.run(
             "pastebin", 
             "get", 
             cfg.link, 
-            cfg.path .. "/" .. name)
-        end
+            cfg.path .. "/" .. name
+        )
     end
 end
 
@@ -26,31 +27,32 @@ function setup()
     shell.run("mkdir", "home")
     shell.run("mkdir", "api_dir")
     print("Created directories..")
-    
+
     installPrograms()
     print("Installed default programs")
-    
+
     if os.computerLabel() == nil then
         os.setComputerLabel("RatCo Micro")
     end
-    
+
     settings.set("motd.enable", false)
     print("Configured settings")
-    
+
     print(
-[[Initialisation complete.
+        [[Initialisation complete.
 Default name: RatCo Micro
 Set computer name with
 label set <name>
-Press [E] to enter ratOs]])
-    
+Press [E] to enter ratOs]]
+    )
+
     -- repeat until E is pressed
     local event, key
     repeat
         event, key = os.pullEvent("key")
     until key == keys.e
     shell.run("clear")
-end    
+end
 
 -- Normal start
 function boot()
@@ -58,18 +60,16 @@ function boot()
     print("Welcome to RatCo Micro")
     print("Current installed programs:")
     -- Loop through every file in bin (k as counter, v as full file path)
-    for k,v in pairs(fs.find("/bin/*")) do
+    for k, v in pairs(fs.find("/bin/*")) do
         -- Everything in bin gets a system-wide alias
-        shell.setAlias(fs.getName(v),"/" .. v)
-        print(k ..".", fs.getName(v))
+        shell.setAlias(fs.getName(v), "/" .. v)
+        print(k .. ".", fs.getName(v))
     end
-    
+
     if fs.exists("autorun") then
         shell.run("background", "autorun")
     end
-
 end
-
 
 if not fs.exists("/bin") then
     setup()
